4c09874c0c9a9e86af17cafd53d451bcdb6d11af
N0I0C0K <nick131410@aliyun.com>
2022-04-20 20:57:34 +0800

    完善了PluginManager，还存在问题

--- src/PluginManager.cpp
+++ src/PluginManager.cpp
@@ -1,9 +1,10 @@
 #include "PluginManager.h"
-#include "Main.h"
 #include "../packets/packets.h"
+#include "Main.h"
 
-PluginManager::PluginManager(juce::File rootPath) : juce::Timer(), knownPluginListXMLFile(rootPath.getChildFile("knownPlugins.xml")),
-	scannerPath(juce::File::getCurrentWorkingDirectory().getChildFile("EIMPluginScanner.exe").getFileName()) {
+PluginManager::PluginManager(juce::File rootPath)
+    : juce::Timer(), knownPluginListXMLFile(rootPath.getChildFile("knownPlugins.xml")),
+      scannerPath(juce::File::getCurrentWorkingDirectory().getChildFile("EIMPluginScanner.exe").getFileName()) {
     manager.addDefaultFormats();
 
     if (knownPluginListXMLFile.exists()) {
@@ -11,84 +12,101 @@ PluginManager::PluginManager(juce::File rootPath) : juce::Timer(), knownPluginLi
         knownPluginList.recreateFromXml(*xml.get());
         xml.reset();
     }
-	auto& cfg = EIMApplication::getEIMInstance()->config;
-	auto obj = cfg.config.getDynamicObject();
-	if (!obj->hasProperty("pluginManager")) {
-		auto obj2 = new juce::DynamicObject();
-		juce::var pluginManager(obj2);
-		if (juce::SystemStats::getOperatingSystemType() == juce::SystemStats::Windows) {
-			obj2->setProperty("scanPaths", juce::StringArray({
-				"C:\\Program Files\\Common Files\\VST3",
-				"C:\\Program Files\\VstPlugins",
-				"C:\\Program Files\\Steinberg\\VSTPlugins"
-			}));
-		}
-		obj2->setProperty("skipFiles", juce::StringArray());
-		obj2->setProperty("thread", 10);
-		obj->setProperty("pluginManager", pluginManager);
-		cfg.changed = true;
-	}
+    auto& cfg = EIMApplication::getEIMInstance()->config;
+    auto obj = cfg.config.getDynamicObject();
+    if (!obj->hasProperty("pluginManager")) {
+        auto obj2 = new juce::DynamicObject();
+        juce::var pluginManager(obj2);
+        if (juce::SystemStats::getOperatingSystemType() == juce::SystemStats::Windows) {
+            obj2->setProperty("scanPaths", juce::StringArray({"C:\\Program Files\\Common Files\\VST3",
+                                                              "C:\\Program Files\\VstPlugins",
+                                                              "C:\\Program Files\\Steinberg\\VSTPlugins"}));
+        }
+        obj2->setProperty("skipFiles", juce::StringArray());
+        obj2->setProperty("thread", 10);
+        obj->setProperty("pluginManager", pluginManager);
+        cfg.changed = true;
+    }
 }
 
-bool PluginManager::isScanning() { return _isScanning; }
+bool PluginManager::isScanning() {
+    return _isScanning;
+}
 
 void PluginManager::timerCallback() {
-	if (!_isScanning) return;
-	for (int i = 0; i < numThreads; i++) {
-		auto& cur = processes[i];
-		if (cur.isRunning()) continue;
-		if (inited) {
-			char arr[10240] = { };
-			try {
-				juce::String str(arr, cur.readProcessOutput(arr, 10240));
-				auto xml = juce::XmlDocument::parse(str);
-				juce::PluginDescription desc;
-				desc.loadFromXml(*xml.release());
-				knownPluginList.addType(desc);
-				DBG("Scanned: " << str);
-			}
-			catch (...) {
-				// TODO
-			}
-		}
-		if (scainngFiles.empty()) {
-			_isScanning = false;
-			stopTimer();
-			delete processes;
-			return;
-		}
-		auto& path = scainngFiles.front();
-		cur.start(juce::StringArray { scannerPath, path });
-		scainngFiles.pop();
-	}
-	inited = true;
-	// TODO: Check process status and relaunch
-	// and finally stopTimer();
+    if (!_isScanning) return;
+    bool hasProcessRunning = false;
+    for (int i = 0; i < numThreads; i++) {
+        auto& cur = processes[i];
+        if (cur.isRunning()) {
+            hasProcessRunning = true;
+            continue;
+        }
+        if (inited) {
+            char arr[10240] = {};
+            try {
+                juce::String str(arr, cur.readProcessOutput(arr, 10240));
+                auto xml = juce::XmlDocument::parse(str);
+                juce::PluginDescription desc;
+                desc.loadFromXml(*xml.release());
+                knownPluginList.addType(desc);
+                DBG("Scanned: " << str);
+            }
+            catch (...) {
+                auto& cfg =
+                    EIMApplication::getEIMInstance()->config.config.getDynamicObject()->getProperty("pluginManager");
+                auto skipFiles = cfg.getProperty("skipFiles", juce::StringArray()).getArray();
+                skipFiles->add(this->processScanFile[i]);
+            }
+        }
+        if (scainngFiles.empty()) {
+            continue;
+        }
+        auto& path = scainngFiles.front();
+        cur.start(juce::StringArray{scannerPath, path});
+        this->processScanFile[i] = path;
+        scainngFiles.pop();
+    }
+    inited = true;
+    if (!hasProcessRunning && scainngFiles.empty()) {
+        stopScanning();
+        return;
+    }
+    // TODO: Check process status and relaunch
+    // and finally stopTimer();
 }
 
 void PluginManager::scanPlugins() {
     if (_isScanning) return;
-	std::thread thread([this]() {
-		auto& cfg = EIMApplication::getEIMInstance()->config.config.getDynamicObject()->getProperty("pluginManager");
-		numThreads = (int)cfg.getProperty("thread", 10);
-		auto pathsArr = cfg.getProperty("scanPaths", juce::StringArray()).getArray();
-		if (pathsArr->isEmpty()) return;
-		juce::FileSearchPath paths;
-		for (auto& it : *pathsArr) paths.add(juce::File(it));
-		paths.removeRedundantPaths();
-		paths.removeNonExistentPaths();
-		std::queue<juce::String> empty;
-		std::swap(scainngFiles, empty);
-		for (auto it : manager.getFormats()) for (auto& path : it->searchPathsForPlugins(paths, true, true)) scainngFiles.emplace(path);
-		_isScanning = true;
-		inited = false;
-		processes = new juce::ChildProcess[numThreads];
-		startTimer(100);
-	});
-	thread.detach();
+    std::thread thread([this]() {
+        auto& cfg = EIMApplication::getEIMInstance()->config.config.getDynamicObject()->getProperty("pluginManager");
+        numThreads = (int)cfg.getProperty("thread", 10);
+        auto pathsArr = cfg.getProperty("scanPaths", juce::StringArray()).getArray();
+        if (pathsArr->isEmpty()) return;
+        juce::FileSearchPath paths;
+        for (auto& it : *pathsArr)
+            paths.add(juce::File(it));
+        paths.removeRedundantPaths();
+        paths.removeNonExistentPaths();
+        std::queue<juce::String> empty;
+        std::swap(scainngFiles, empty);
+        for (auto it : manager.getFormats())
+            for (auto& path : it->searchPathsForPlugins(paths, true, true))
+                scainngFiles.emplace(path);
+        _isScanning = true;
+        inited = false;
+        processes = new juce::ChildProcess[numThreads];
+        processScanFile.resize(numThreads);
+        processScanFile.clear();
+        startTimer(100);
+    });
+    thread.detach();
 }
 
 void PluginManager::stopScanning() {
-	if (!_isScanning) return;
-	_isScanning = false;
+    if (!_isScanning) return;
+    stopTimer();
+    _isScanning = false;
+    knownPluginList.createXml().release()->writeTo(knownPluginListXMLFile);
+    delete processes;
 }
--- src/PluginManager.h
+++ src/PluginManager.h
@@ -3,24 +3,25 @@
 #include <juce_audio_utils/juce_audio_utils.h>
 
 class PluginManager : juce::Timer {
-public:
+  public:
     PluginManager(juce::File rootPath);
 
     juce::AudioPluginFormatManager manager;
     juce::KnownPluginList knownPluginList;
 
     void scanPlugins();
-	void stopScanning();
-	void timerCallback() override;
-	bool isScanning();
-private:
-	int numThreads = 10;
-	bool _isScanning = false;
-	bool inited = false;
-    const juce::File knownPluginListXMLFile;
-	juce::String scannerPath;
-	std::queue<juce::String> scainngFiles;
-	juce::ChildProcess* processes = nullptr;
+    void stopScanning();
+    void timerCallback() override;
+    bool isScanning();
 
+  private:
+    int numThreads = 10;
+    bool _isScanning = false;
+    bool inited = false;
+    const juce::File knownPluginListXMLFile;
+    juce::String scannerPath;
+    std::queue<juce::String> scainngFiles;
+    juce::ChildProcess* processes = nullptr;
+    std::vector<juce::String> processScanFile;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PluginManager)
 };
